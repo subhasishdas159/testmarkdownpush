a1a28067a36c8deecb79e2ff60f550e2b206ecdd
Subhasish Das <subhasish@doc32.in>
2022-06-04 13:18:25 +0530

    hello this
    is another test commit
    
    okay

--- gitDemo.mjs
+++ gitDemo.mjs
@@ -6,12 +6,14 @@ var commitMsgArgs = process.argv.slice(2);
 // after it has been init this runs
 const afterInit = async () => {
   const status = await git.status();
-  console.log('status', status)
+  console.log('status', status);
   // Only run this when there is actually some changes
   if (!status.isClean()) {
-    await git.add('./*').commit(commitMsgArgs, () =>
-      console.log('after committing this has ben invoked'),
-    );
+    await git
+      .add('./*')
+      .commit(commitMsgArgs, () =>
+        console.log('after committing this has ben invoked'),
+      );
   }
   console.log('here');
 };
@@ -21,5 +23,9 @@ try {
 } catch (error) {
   console.log('error', error);
   await git.init();
+  await git.addRemote(
+    'origin',
+    'git@github.com:subhasishdas159/testmarkdownpush.git',
+  );
   await afterInit();
 }
